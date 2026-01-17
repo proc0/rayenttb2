ccecb03eac90409a7f4fd2ba56acc0be7d167477
proc0 <alanvx@gmail.com>
2026-01-17 04:39:29 -0800

    cmake cleanup

--- include/config.h
+++ include/config.h
@@ -3,5 +3,5 @@
 #define PROJECT_NAME "rayenttb2"
 #define VERSION "0.1.0"
 #define DEBUG 1
-#define DATE_BUILD "2026-01-16 00:23:25"
+#define DATE_BUILD "2026-01-16 07:44:38"
 #define DIR_ASSETS "assets"
