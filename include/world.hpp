46bfcf298af114234dda18dfaa3832485c6a9f57
proc0 <alanvx@gmail.com>
2026-01-18 06:38:23 -0800

    adding basic world and ground and test elements for box2D

--- include/world.hpp
+++ include/world.hpp
@@ -1,21 +1,91 @@
 #pragma once
 
+#include <string>
 #include <raylib.h>
 #include <entt/entt.hpp>
-#include <string>
+#include <box2d/id.h>
+#include <box2d/types.h>
+#include <box2d/box2d.h>
+#include <box2d/math_functions.h>
 
 #include "config.h"
 
 #define URI_SOUND_SPLAT "splat1.wav"
+#define ENT_COUNT 20
+#define RAND_LIMIT 32767
+#define RAND_SEED 12345
+
+inline uint32_t g_randomSeed;
+// Simple random number generator. Using this instead of rand() for cross-platform determinism.
+B2_INLINE int RandomInt()
+{
+    // XorShift32 algorithm
+    uint32_t x = g_randomSeed;
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 5;
+    g_randomSeed = x;
+
+    // Map the 32-bit value to the range 0 to RAND_LIMIT
+    return (int)( x % ( RAND_LIMIT + 1 ) );
+}
+
+// Random number in range [-1,1]
+B2_INLINE float RandomFloat()
+{
+    float r = (float)( RandomInt() & ( RAND_LIMIT ) );
+    r /= RAND_LIMIT;
+    r = 2.0f * r - 1.0f;
+    return r;
+}
+
+// Random floating point number in range [lo, hi]
+B2_INLINE float RandomFloatRange( float lo, float hi )
+{
+    float r = (float)( RandomInt() & ( RAND_LIMIT ) );
+    r /= RAND_LIMIT;
+    r = ( hi - lo ) * r + lo;
+    return r;
+}
+
+void DrawPolygonFcn( const b2Vec2* vertices, int vertexCount, b2HexColor color, void* context );
+
+void DrawSolidPolygonFcn( b2Transform transform, const b2Vec2* vertices, int vertexCount, float radius, b2HexColor color,
+                          void* context );
+
+void DrawCircleFcn( b2Vec2 center, float radius, b2HexColor color, void* context );
+
+void DrawSolidCircleFcn( b2Transform transform, float radius, b2HexColor color, void* context );
+
+void DrawSolidCapsuleFcn( b2Vec2 p1, b2Vec2 p2, float radius, b2HexColor color, void* context );
+
+void DrawLineFcn( b2Vec2 p1, b2Vec2 p2, b2HexColor color, void* context );
+
+void DrawTransformFcn( b2Transform transform, void* context );
+
+void DrawPointFcn( b2Vec2 p, float size, b2HexColor color, void* context );
+
+void DrawStringFcn( b2Vec2 p, const char* s, b2HexColor color, void* context );
 
 typedef struct Position { float x; float y; } Position;
 typedef struct Velocity { float x; float y; } Velocity;
 typedef struct ParticleTag {} ParticleTag;
 
+struct BodyUserData
+{
+    int index;
+};
+
 class World {
-    int count_;
+    b2BodyId m_debrisIds[ENT_COUNT];
+    BodyUserData m_bodyUserData[ENT_COUNT];
     Sound splat;
-    
+    b2WorldId worldId = b2_nullWorldId;
+    b2DebugDraw debugDraw;
+    float timeStep = 1.0f/60.0f;
+    int count_;
+    short entCount = 20;
+
     public:
     int screenWidth;
     int screenHeight;
@@ -27,6 +97,9 @@ class World {
     
     int count();
     void load();
+    void createWorld();
+    void createGround();
+    void spawnDebris();
     void render() const;
     entt::entity spawnParticle(float x, float y);
     void update();
--- src/world.cpp
+++ src/world.cpp
@@ -1,5 +1,50 @@
 #include "world.hpp"
-#include "raylib.h"
+
+void DrawPolygonFcn( const b2Vec2* vertices, int vertexCount, b2HexColor color, void* context ) {
+    // SampleContext* sampleContext = static_cast<SampleContext*>( context );
+    // DrawPolygon( sampleContext->draw, vertices, vertexCount, color );
+}
+
+void DrawSolidPolygonFcn( b2Transform transform, const b2Vec2* vertices, int vertexCount, float radius, b2HexColor color,
+                          void* context ) {
+    // SampleContext* sampleContext = static_cast<SampleContext*>( context );
+    // DrawSolidPolygon( sampleContext->draw, transform, vertices, vertexCount, radius, color );
+}
+
+void DrawCircleFcn( b2Vec2 center, float radius, b2HexColor color, void* context ) {
+    // SampleContext* sampleContext = static_cast<SampleContext*>( context );
+    // DrawCircle( sampleContext->draw, center, radius, color );
+}
+
+void DrawSolidCircleFcn( b2Transform transform, float radius, b2HexColor color, void* context ) {
+    // SampleContext* sampleContext = static_cast<SampleContext*>( context );
+    // DrawSolidCircle( sampleContext->draw, transform, radius, color );
+}
+
+void DrawSolidCapsuleFcn( b2Vec2 p1, b2Vec2 p2, float radius, b2HexColor color, void* context ) {
+    // SampleContext* sampleContext = static_cast<SampleContext*>( context );
+    // DrawSolidCapsule( sampleContext->draw, p1, p2, radius, color );
+}
+
+void DrawLineFcn( b2Vec2 p1, b2Vec2 p2, b2HexColor color, void* context ) {
+    // SampleContext* sampleContext = static_cast<SampleContext*>( context );
+    // DrawLine( sampleContext->draw, p1, p2, color );
+}
+
+void DrawTransformFcn( b2Transform transform, void* context ) {
+    // SampleContext* sampleContext = static_cast<SampleContext*>( context );
+    // DrawTransform( sampleContext->draw, transform, 1.0f );
+}
+
+void DrawPointFcn( b2Vec2 p, float size, b2HexColor color, void* context ) {
+    // SampleContext* sampleContext = static_cast<SampleContext*>( context );
+    // DrawPoint( sampleContext->draw, p, size, color );
+}
+
+void DrawStringFcn( b2Vec2 p, const char* s, b2HexColor color, void* context ) {
+    // SampleContext* sampleContext = static_cast<SampleContext*>( context );
+    // DrawWorldString( sampleContext->draw, &sampleContext->camera, p, color, s );
+}
 
 void MovementSystem(World& world) {
     auto view = world.registry().view<Position, Velocity>();
@@ -21,6 +66,112 @@ void World::load(){
     splat = LoadSound(pathSoundSplat);
     count_ = 0;
     spawnParticle(100.0f, 100.0f);
+
+    debugDraw = b2DefaultDebugDraw();
+    debugDraw.DrawPolygonFcn = DrawPolygonFcn;
+    debugDraw.DrawSolidPolygonFcn = DrawSolidPolygonFcn;
+    debugDraw.DrawCircleFcn = DrawCircleFcn;
+    debugDraw.DrawSolidCircleFcn = DrawSolidCircleFcn;
+    debugDraw.DrawSolidCapsuleFcn = DrawSolidCapsuleFcn;
+    debugDraw.DrawLineFcn = DrawLineFcn;
+    debugDraw.DrawTransformFcn = DrawTransformFcn;
+    debugDraw.DrawPointFcn = DrawPointFcn;
+    debugDraw.DrawStringFcn = DrawStringFcn;
+    createWorld();
+    createGround();
+
+    for (int i = 0; i < ENT_COUNT; ++i)
+    {
+        m_debrisIds[i] = b2_nullBodyId;
+        m_bodyUserData[i].index = i;
+        spawnDebris();
+    }
+}
+
+void World::createWorld()
+{
+    if ( B2_IS_NON_NULL( worldId ) )
+    {
+        b2DestroyWorld( worldId );
+        worldId = b2_nullWorldId;
+    }
+
+    b2WorldDef worldDef = b2DefaultWorldDef();
+    // worldDef.workerCount = context->workerCount;
+    // worldDef.enqueueTask = EnqueueTask;
+    // worldDef.finishTask = FinishTask;
+    // worldDef.userTaskContext = this;
+    // worldDef.enableSleep = context->enableSleep;
+
+    // todo experimental
+    // worldDef.enableContactSoftening = true;
+
+    worldId = b2CreateWorld( &worldDef );
+}
+
+void World::createGround() {
+    b2BodyDef bodyDef = b2DefaultBodyDef();
+    b2BodyId groundId = b2CreateBody( worldId, &bodyDef );
+
+    b2Vec2 points[] = { { 40.0f, -40.0f }, { -40.0f, -40.0f }, { -40.0f, 40.0f }, { 40.0f, 40.0f } };
+
+    b2ChainDef chainDef = b2DefaultChainDef();
+    chainDef.count = 4;
+    chainDef.points = points;
+    chainDef.isLoop = true;
+
+    b2CreateChain( groundId, &chainDef );
+}
+
+void World::spawnDebris()
+{
+    int index = -1;
+    for (int i = 0; i < ENT_COUNT; ++i)
+    {
+        if (B2_IS_NULL( m_debrisIds[i] ))
+        {
+            index = i;
+            break;
+        }
+    }
+
+    if (index == -1)
+    {
+        return;
+    }
+
+    // Debris
+    b2BodyDef bodyDef = b2DefaultBodyDef();
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position = { RandomFloatRange( -38.0f, 38.0f ), RandomFloatRange( -38.0f, 38.0f ) };
+    bodyDef.rotation = b2MakeRot( RandomFloatRange( -B2_PI, B2_PI ) );
+    bodyDef.linearVelocity = { RandomFloatRange( -5.0f, 5.0f ), RandomFloatRange( -5.0f, 5.0f ) };
+    bodyDef.angularVelocity = RandomFloatRange( -1.0f, 1.0f );
+    bodyDef.gravityScale = 0.0f;
+    bodyDef.userData = m_bodyUserData + index;
+    m_debrisIds[index] = b2CreateBody( worldId, &bodyDef );
+
+    b2ShapeDef shapeDef = b2DefaultShapeDef();
+    shapeDef.material.restitution = 0.8f;
+
+    // No events when debris hits debris
+    shapeDef.enableContactEvents = false;
+
+    if (( index + 1 ) % 3 == 0)
+    {
+        b2Circle circle = { { 0.0f, 0.0f }, 0.5f };
+        b2CreateCircleShape( m_debrisIds[index], &shapeDef, &circle );
+    }
+    else if (( index + 1 ) % 2 == 0)
+    {
+        b2Capsule capsule = { { 0.0f, -0.25f }, { 0.0f, 0.25f }, 0.25f };
+        b2CreateCapsuleShape( m_debrisIds[index], &shapeDef, &capsule );
+    }
+    else
+    {
+        b2Polygon box = b2MakeBox( 0.4f, 0.6f );
+        b2CreatePolygonShape( m_debrisIds[index], &shapeDef, &box );
+    }
 }
 
 void World::render() const {
@@ -56,6 +207,8 @@ void World::update(){
     }
 
     MovementSystem(*this);
+    b2World_Step( worldId, timeStep, 4 );
+    b2World_Draw( worldId, &debugDraw );
 }
 
 void World::unload(){
